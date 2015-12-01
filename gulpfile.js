34913979ff5841f028329c2d01dfabe86e028aea
Christopher Brown <christopher.p.brown@gmail.com>
2015-12-01 08:31:51 -0700

    restructuring

--- gulpfile.js
+++ gulpfile.js
@@ -6,25 +6,24 @@ var sass = require('gulp-sass')
 
 gulp.task('html', function(){
   return gulp.src('src/index.html')
-    .pipe(gulp.dest('dist'))
+    .pipe(gulp.dest('.'))
 });
 
 gulp.task('styles', function(){
-  return gulp.src('src/sass/*.scss')
+  return gulp.src('src/sass/main.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(concat('main.css'))
-    .pipe(gulp.dest('dist'))
+    .pipe(gulp.dest('./css'))
 });
 
 gulp.task('scripts', function(){
   return browserify('src/scripts/app.js')
     .bundle()
     .pipe(source('main.js'))
-    .pipe(gulp.dest('dist'))
+    .pipe(gulp.dest('./js'))
 });
 
 gulp.task('watchman', function(){
-  gulp.watch('src/scrips/*js', ['scripts'])
+  gulp.watch('src/scripts/*js', ['scripts'])
   gulp.watch('src/sass/*scss', ['styles'])
   gulp.watch('src/index.html' ['html'])
 });
